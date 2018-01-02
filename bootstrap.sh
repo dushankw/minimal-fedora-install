49ecb4a2a9e84839763c56245c225460d4f7933e
Dushan <dushan.kw@rea-group.com>
2018-01-02 23:19:55 +1100

    some additional tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ set -euo pipefail
 dnf -y install \
     glx-utils \
     mesa-dri-drivers \
+    mesa-vulkan-drivers \
     plymouth-system-theme \
     xorg-x11-drv-evdev \
     xorg-x11-drv-fbdev \
@@ -45,6 +46,14 @@ dnf -y install \
     liberation-sans-fonts \
     liberation-serif-fonts
 
+# Optional Fonts for Asian languages (you'll encounter ugliness in browsers without these)
+dnf -y install \
+    adobe-source-han-sans-cn-fonts \
+    adobe-source-han-serif-cn-fonts \
+    google-noto-sans-thai-fonts \
+    google-noto-serif-thai-fonts \
+    un-core-batang-fonts
+
 # Codecs
 dnf -y install \
     gstreamer1 \
@@ -52,6 +61,18 @@ dnf -y install \
     gstreamer1-plugins-bad-free-gtk \
     gstreamer1-plugins-base \
     gstreamer1-plugins-good
+
+# Sound
+dnf -y install \
+    pulseaudio \
+    pulseaudio-libs \
+    pulseaudio-utils \
+    alsa-plugins-pulseaudio \
+    pavucontrol
+
+# Power
+dnf -y install \
+    upower
 
 # Set graphical target
 systemctl enable lightdm.service
