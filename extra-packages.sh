a056fb631ac15b8fa5a03c08308d4591e1a3540d
Dushan <dushankw@gmail.com>
2020-05-12 15:34:00 +1000

    zbar (zbarimg) is a useful tool for reading qr codes

--- extra-packages.sh
+++ extra-packages.sh
@@ -93,7 +93,8 @@ sudo dnf install \
     xar \
     yubikey-manager \
     yubikey-personalization-gui \
-    redshift
+    redshift \
+    zbar
 
 # The following are also good, but come from rpmfusion, install it if you want them
 # https://rpmfusion.org/Configuration (be sure to verify GPG signatures)
